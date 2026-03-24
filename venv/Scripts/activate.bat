7eec06c73860b636f5ee24d015080d9247792164
alvesssjean <jeanpaulo370@gmail.com>
2026-03-24 19:41:40 -0300

    criacao das rotas update e delete

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ALUNO\Documents\aula18.03\banco_dados_nao_relacional\venv
+set VIRTUAL_ENV=C:\Users\ALUNO\Documents\aula24.03\banco_dados_nao_relacional\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
